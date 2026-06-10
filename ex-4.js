1436fb78ec62b733baa2d24888ac4730f4e47923
CodePawsDev <isripen1991@outlook.com>
2026-06-10 18:13:17 +0700

    feat: add ex-1 to ex-8 and concept.js

---
+++ ex-4.js
@@ -0,0 +1,34 @@
+/* 
+ให้
+1. แสดงข้อมูลว่า James ชอบทำกิจกรรมอะไร ในรูปแบบ James likes Playing Football.
+2. แสดงข้อมูลว่า John ชอบทำกิจกรรมอะไร ในรูปแบบ John likes Playing Computer Games.
+3. แสดงข้อมูลว่า Q มี id เท่ากับเท่าไร ในรูปแบบ Q's id is 3
+4. ให้เปลี่ยนข้อมูล hobby ของ Q เป็น Singing
+5. แสดงข้อมูลว่า Q ชอบทำกิจกรรมอะไร ในรูปแบบ Q likes Singing.
+6. ให้เพิ่มข้อมูลนักเรียนเข้าไปอีกคน นักเรียนคนนี้มี id = 5 ชื่อ Lisa และชอบ Dancing 
+7. ให้แสดงข้อมูลของ Lisa ออกมาในรูปแบบ Lisa's id is 5. Lisa likes Dancing.
+8. ให้ลบข้อมูลของ Lisa ออก และแสดงจำนวนสมาชิกใน students ต้องเห็นว่ามีสมาชิกเท่ากับ 4
+*/
+
+const students = [
+  {
+    id: 1,
+    name: "James",
+    hobby: "Playing Football"
+  },
+  {
+    id: 2,
+    name: "JJ",
+    hobby: "Coding"
+  },
+  {
+    id: 3,
+    name: "Q",
+    hobby: "Playing Guitar"
+  },
+  {
+    id: 4,
+    name: "John",
+    hobby: "Playing Computer Games"
+  }
+];
---
+++ ex-7.js
@@ -0,0 +1,13 @@
+/* ให้เขียน function เพื่อหาว่าคะแนนต่ำสุดเท่ากับเท่าไร */
+
+function findMinScore (arr) {
+    // เขียนโค้ดตรงนี้
+}
+
+const roomOneScores = [70, 65, 70, 80, 95, 100];
+const roomTwoScore = [80, 55, 54, 74, 87, 98, 100, 45, 10];
+
+console.log(findMinScore(roomOneScores)) // 65
+console.log(findMinScore(roomTwoScore)) // 10
+
+
